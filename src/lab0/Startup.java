a9ce08dab683db0b13b686a75cbb3c358d637383
ajayasooriya <ajayasooriya@L11710042773.wctc.msft>
2017-04-03 13:29:26 -0500

    Second commit.

--- src/lab0/Startup.java
+++ src/lab0/Startup.java
@@ -27,7 +27,7 @@ public class Startup {
         // if there's a crash. In this example the program does not crash and
         // the user gets a nice, friendly message.
         try {
-            emp = new Employee("John","Doe","33333333u",28);
+            emp = new Employee("John","Doe","333333333",28);
             emp.setDaysVacation(1);
             System.out.println(emp.getLastName());
 //            String fullNameInCaps = 
